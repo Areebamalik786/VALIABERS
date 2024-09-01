ff5692bd204c47d5d3fee746de0527e9da1dd2ca
Areeba Malik <areebamalik031877>
2024-09-01 13:23:52 +0500

    third commit

---
+++ main.js
@@ -0,0 +1,33 @@
+"use strict";
+// VARIABLES
+let personName = "Areeba";
+//console.log(personName)
+personName = "Habiba";
+console.log(personName);
+//DATA TYPES;
+// 1.String
+let ColourName = "Green";
+//console.log (ColourName)
+ColourName = "white";
+console.log(ColourName);
+// 2.Number
+let age = 26;
+//console.log(age)
+age = 16;
+console.log(age);
+let a = 8;
+let b = 6;
+console.log(a + b);
+// 3.Boolean
+let Username = true;
+let Username1 = false;
+// 4.Any
+let countryName1 = "pakistan";
+console.log(countryName1);
+let countryNumber = 67;
+console.log(countryNumber);
+let d = 67;
+let e = 78;
+//console.log(d ==e)
+console.log(d < e);
+console.log(d > e);
---
+++ main.ts
@@ -0,0 +1,53 @@
+// VARIABLES
+let personName = "Areeba";
+//console.log(personName)
+personName= "Habiba"
+console.log(personName)
+
+//DATA TYPES;
+
+// 1.String
+let ColourName : String ="Green";
+//console.log (ColourName)
+ColourName = "white";
+console.log(ColourName)
+
+// 2.Number
+let age : number = 26;
+//console.log(age)
+age = 16;
+console.log(age)
+let a : number = 8;
+let b :number = 6;
+console.log(a+b)
+
+// 3.Boolean
+let Username : boolean = true
+let Username1 : boolean = false
+
+// 4.Any
+let countryName1: any ="pakistan"
+console.log(countryName1)
+let countryNumber : any = 67;
+console.log(countryNumber)
+let d : any = 67;
+let e : any = 78;
+//console.log(d ==e)
+console.log(d <e)
+console.log(d > e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
